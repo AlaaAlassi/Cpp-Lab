7129ea95cfd7ff32455b72ef483421aceb9d82b3
Alaa Alassi <alaa.alassi@nobleo.nl>
2021-11-20 00:32:22 +0100

    add set name

--- use_heap_alocated_object.cpp
+++ use_heap_alocated_object.cpp
@@ -11,21 +11,34 @@ public:
         std::cout << "Vehicle ID=" << _id << std::endl;
     }
 
+    void setName(std::string name){
+        _name = name;
+    }
+
+    void printName()
+    {
+        std::cout << "Vehicle name is" << _name << std::endl;
+    }
+
 private:
     int _id;
+    std::string _name;
 };
 
 int main()
 {
     // create thread
     std::shared_ptr<Vehicle> v(new Vehicle);
-    std::thread t = std::thread(&Vehicle::addID, v, 1); // call member function on object v
+    std::thread t1 = std::thread(&Vehicle::addID, v, 1); // call member function on object v
+    std::thread t2 = std::thread(&Vehicle::setName, v, "opel");
 
     // wait for thread to finish
-    t.join();
+    t1.join();
+    t2.join();
 
     // print Vehicle id
     v->printID();
+    v->printName();
 
     return 0;
 }
